bab3a0fc825fbf0d820ffa50c3e7ce42484f96cb
hensh <shorc@post.bgu.ac.il>
2025-06-25 16:22:46 +0300

    updated directly to main because already submited but noticed a little impreovement

---
+++ Frontend/Utils/VisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Frontend.Utils
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolValue = (bool)value;
+            bool invert = parameter?.ToString() == "invert";
+            if (invert)
+                boolValue = !boolValue;
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
